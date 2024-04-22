d4aa63843b994a40ee4971fdd16e265cd66d30c0
VidhyaVijayan08 <165891201+VidhyaVijayan08@users.noreply.github.com>
2024-04-22 15:33:12 +0530

    Add files via upload

---
+++ bloodbank.sql
@@ -0,0 +1,10 @@
+SELECT * FROM bloodbank;
+INSERT INTO bloodbank VALUES(1,"Vidhya","O+");
+INSERT INTO bloodbank VALUES(2,"Santhini","O+");
+INSERT INTO bloodbank VALUES(3,"Swetha","A+");
+INSERT INTO bloodbank VALUES(4,"Swarna","AB-");
+INSERT INTO bloodbank VALUES(5,"Sangeetha","A-");
+UPDATE bloodbank SET name="Sangi" WHERE id=5;
+
+
+
